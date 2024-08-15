0490036bf5c0b4006062acf9ebca11c45e3b720d
Eduarda <112082862+eduardamarqueszxd@users.noreply.github.com>
2024-08-15 16:56:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,113 @@
-const Caixaprincipal = document.querySelector(".caixa-principal");
-const CaixaPerghuntas = document.querySelector(".caixa-perguntas");
-const CaixaAlternativas = document.querySelector(".caixa-alternativas");
-const CaixaResultado = document.querySelector(".caixa-resultados");
-const TextoResultado = document.querySelector(".Texto-resultados");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
+        enunciado: "Assinale a alternativa que apresenta a melhor conceituação do termo meio ambiente:",
+        alternativas: [
+            {
+                texto: " É a inter-relação entre os diversos componentes físicos e humanos",
+                afirmacao: "Correta"
+            },
+            {
+                texto: "É formado pelos elementos que foram construídos pela sociedade.",
+                afirmacao: "Errada"
+            }
+        ]
+    },
+    {
         enunciado: "Assinale a alternativa que NÃO apresenta um problema ambiental:",
         alternativas: [
-            "Caça de diversos animais silvestres.",
-            "Infiltração da água da chuva no solo."
+            {
+                texto: "Retirada da vegetação nativa local.",
+                afirmacao: "Errada"
+            },
+            {
+                texto: " Infiltração da água da chuva no solo.",
+                afirmacao: "Correta"
+            }
         ]
     },
     {
         enunciado: "Qual conceito ligado ao meio ambiente defende a manutenção das características naturais do espaço natural, ou seja, sem nenhum tipo de intervenção antrópica?",
         alternativas: [
-            "Conservação ambiental.",
-            " Preservação ambiental."
+            {
+                texto: "Preservação ambiental.",
+                afirmacao: "Corrreta"
+            },
+            {
+                texto: "comunidade.",
+                afirmacao: "Errada"
+            }
         ]
     },
     {
         enunciado: "O conceito de conservação ambiental implica no uso racional dos recursos naturais. Desse modo, ele está diretamente ligado ao conceito de",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "preservação ",
+                afirmacao: "Errada"
+            },
+            {
+                texto: " sustentabilidade.",
+                afirmacao: "Correta"
+            }
         ]
     },
     {
-        enunciado: "Pergunta 4",
+        enunciado: "Assinale a alternativa que apresenta uma característica do espaço ambiental brasileiro:",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Ocorrência de graves impactos ambientais no meio natural.",
+                afirmacao: "Correta"
+            },
+            {
+                texto: "Pequena diversidade geográfica do espaço físico local.",
+                afirmacao: "Errada"
+            }
         ]
     },
-    {
-        enunciado: "Pergunta 5",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
-    }
 ];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
